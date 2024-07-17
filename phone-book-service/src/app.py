098214fbc16da7934489b669ea04b1292277671c
mrflick72 <valerio.vaudi@gmail.com>
2024-07-17 19:42:20 +0200

    add health endpoint

--- phone-book-service/src/app.py
+++ phone-book-service/src/app.py
@@ -1,18 +1,20 @@
 from flask import Flask
 
-from infrastructure.AppConfig import application_init
+from src.infrastructure.AppConfig import application_init
+from src.web.UserNameInjectorFilter import UserNameInjectorFilter
 from web.HealthEndPoint import HealthEndPoint
-from web.UserNameInjectorFilter import UserNameInjectorFilter
 
 app = Flask(__name__)
-actuatorApp = Flask(__name__)
 
-HealthEndPoint(actuatorApp)
-user_nameInjector_filter = UserNameInjectorFilter()
-app.before_request(user_nameInjector_filter.filter)
+user_name_injector_filter = UserNameInjectorFilter()
+app.before_request(user_name_injector_filter.filter)
 application_init(app)
+HealthEndPoint(app)
+
 
 if __name__ == '__main__':
-    port = 5000
     app.run(port=5000)
-    actuatorApp.run(port=port + 1)
+
+
+
+
